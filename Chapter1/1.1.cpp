3263a8a6792f95be584d2efc2a37be27e5a9d8ae
Ruan Xun <galaxymaxx@outlook.com>
2017-12-05 01:41:37 -0500

    Test clog, cout and cerr

--- Chapter1/1.1.cpp
+++ Chapter1/1.1.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+	// Test cout, cerr and clog
 	cout << "Hello world" << endl;
+	cerr << "error message" << endl;
+	clog << "program about to end" << endl;
 	return -1;
 }
